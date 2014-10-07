7325c09f88f944a8c6580aed58628c6e12e30470
tonypai <tony77794@gmail.com>
2014-10-07 23:18:11 +0800

    v3.0.1 * Xuite 疑似不能下載 >>已加上 loading 提醒，因為有設定兩台伺服器，第一台掛掉的時候會連備用站，因此會多花點時間。 >>另外發現網址尾端 vid 如果有類似 # 的符號會無法正確解析影片位址，這部分也已經修正。

--- target/xuite.js
+++ target/xuite.js
@@ -3,32 +3,40 @@ var backup = "www2.thu.edu.tw/~dataprt";
 var domain = server;
 
 $(document).ready(function() {
+	
+	$("#single-video-operation").append(a_tag("loading", "#", "影音瑞士刀解析中請稍待..."))
+
 	xhrRequest()
 })
 
 var xhrRequest = function () {
 	var s_url = location.href.split("/")
-	var vid = s_url[4]
+	var vid = s_url[4].replace(/#/ig, "")
 	var filename = atob(vid)
 	var m_filename = filename.match(/\w{1,}-(\d{1,}).flv/)
 	var media_id = btoa(m_filename[1])
 	
 	var url = "http://vlog.xuite.net/flash/player?media="+media_id
 	var xhr = new XMLHttpRequest()
+
 	xhr.onreadystatechange = function() {
 		if (xhr.readyState == 4) {
 			if (xhr.status == 200) {
+				$("#loading").remove()
 				var result = xhr.responseText;
 				var m_flv_src = result.match(/<property id="Zmx2X3NyYw=="><!\[CDATA\[([^\]]*)/)
 				var flv_src = decodeURIComponent(atob(m_flv_src[1]))
-				$("#single-video-operation").append("<a href='"+flv_src+"' class='single-video-tool' target='_blank'><span class='single-button-text' style='padding-left: 0px;'>下載收藏</span></a>")
+				$("#single-video-operation").append(a_tag("target", flv_src, "下載收藏"))
 			} else {
 				domain = backup
 				xhrRequest()
-				console.log(xhr.status);
 			}
 		}
 	};
 	xhr.open("GET", "http://"+domain+"/file_get_contents.php?url="+encodeURIComponent(url), true)
 	xhr.send(null)
+};
+
+var a_tag = function (id, url, text) {
+	return "<a id='"+id+"' href='"+url+"' class='single-video-tool'><span class='single-button-text' style='padding-left: 0px;'>"+text+"</a>"
 };
