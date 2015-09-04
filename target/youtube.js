5567d3a2ffbe3c12a4e2874b08fcbe7d9ac509bf
tonypai <tony77794@gmail.com>
2015-09-04 22:44:08 +0800

    v3.2.0 Youtube無法下載 >>已修復 Dailymotion下載連結消失 >>已修復 二次元之門故障中 >>有空再修 停止支援Yam影音 >>沒什麼人在用

--- target/youtube.js
+++ target/youtube.js
@@ -1,31 +1,39 @@
-$(document).ready(function() {
-	for(var j=0;j<document.scripts.length;j++) {
-		if(matched = document.scripts[j].innerHTML.match(/ytplayer.config = [^@]*/)) {
-			var yt = JSON.parse(matched[0].replace(/ytplayer.config = /g, "").replace(/;ytplayer.load[^$]*/, ""));
-			
-			var url_arr = yt.args.url_encoded_fmt_stream_map.split(",")
-			var each_fmt = yt.args.fmt_list.replace(/\\\//g, "/").split(",")
-			var panel = ''
-			
-			for(var i=0;i<url_arr.length;i++)
-			{
-				if(each_fmt[i-1] != undefined)
-				{
-					var title = '&title='+document.getElementById('watch-headline-title').innerHTML.replace(/(<([^>]+)>)/ig,"").trim().replace(/ /g, "+");
-					var tag = getParameterByName("&"+url_arr[i], "type");
-					var url = decodeURIComponent(url_arr[i].match(/url=([^$]*)/)[1]) + title;
-					var size = each_fmt[i-1].split('/');
-					panel += ''+
-					'<span>'+
-					'  <button class="yt-uix-button yt-uix-button-size-default yt-uix-button-opacity yt-uix-button-has-icon action-panel-trigger   yt-uix-button-opacity yt-uix-tooltip" type="button" onclick=";return false;" title="" data-trigger-for="action-panel-share" data-button-toggle="true">'+
-					'	<span class="yt-uix-button-content">'+
-					'	<a href='+url+' title='+tag.match(/[^;]*/)[0].replace(/video\//g, "")+'>'+size[1]+'</a>'+
-					'	</span>'+
-					'  </button>'+
-					'</span>';
-				}
-			}
-			document.getElementById('watch8-secondary-actions').innerHTML += panel;
-		}
-	}
+var done = false
+$(window).load(function() {
+	chk ()
 })
+
+$("a[rel='spf-prefetch']").click(function() {
+	chk ()
+})
+
+var chk = function () {
+	var job = setInterval(function() {
+		for(var j=0;j<document.scripts.length;j++) {
+			if(matched = document.scripts[j].innerHTML.match(/ytplayer.config = [^@]*/)) {
+				var yt = JSON.parse(matched[0].replace(/ytplayer.config = /g, "").replace(/;ytplayer.load[^$]*/, ""))
+				var map = decodeURIComponent(yt.args.url_encoded_fmt_stream_map)
+				var url_arr = map.match(/url=[^,]*/ig)
+				var result = []
+				var type_arr = map.match(/type=[^,;&]*/ig)
+				$.each (type_arr, function(key, val) {
+					result.push({id: key, type: val.substr(5).match(/\/([a-z,1-9,-]{1,})/)[1], url: ""})
+				})
+				$.each (url_arr, function(key, val) {
+					$.each (result, function(k, v) {
+						if (v.id == key)
+							v.url = val.substr(4)
+					})
+				})
+				var title = $("#eow-title").text()
+				$("#eow-title").append("<br />")
+				$.each (result, function(key, val) {
+					$("#eow-title").append("<a href='"+val.url+"&title="+title+"' target='_blank' style='color: #167ac6;'>"+val.type+"</a>")
+					if (key != result.length - 1)$("#eow-title").append(" | ")
+				})
+				done = true
+			}
+		}
+		if (done)clearInterval(job)
+	}, 1000)
+}
